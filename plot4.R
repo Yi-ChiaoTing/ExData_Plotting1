93d714ebb19beeaf6a56bbe81b4e512e1ce3b9ec
Yi-Chiao Susan Ting <30495127+Yi-ChiaoTing@users.noreply.github.com>
2017-09-12 18:47:56 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,16 +1,20 @@
+#Download and read the file
 if(!file.exists("./data")){dir.create("./data")}
 URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(URL, destfile = "Dataset.zip")
 unzip(zipfile = "./data/Dataset.zip",exdir = "./data")
 
+#subset data
 bigdata <- read.table("household_power_consumption.txt", header=TRUE, na.strings="?", sep=";")
 data <- bigdata[(bigdata$Date=="1/2/2007" | bigdata$Date=="2/2/2007" ), ]
 
+#set date and time
 data$Date <- as.Date(data$Date,format="%d/%m/%Y")
 data$time <- strptime(data$Time,format = "%H:%M:%S")
 DateTime <- paste(data$Date, data$Time)
 data$DateTime <- as.POSIXct(DateTime)
 
+#plot and store the file in png device
 par(mfrow=c(2,2),mar=c(4,4,2,1),oma=c(0,0,1,0))
 
 with(data, {
